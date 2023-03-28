365f3053929c8ea4711543448d12ceffc2090bc6
shiroosaeed <shiroosaeed@gmail.com>
2023-03-28 14:04:37 +0430

    v1

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
 from flask import Flask, render_template,jsonify
 import os
 import json
+
+from helpers import is_active
 app = Flask(__name__)
 
 app.config['template_engine'] = None
+ 
 
+# load template section
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', is_active=is_active)
 
-@app.route('/users')
+@app.route('/users_list')  
+def users_list():
+     return render_template('users-list.html', is_active=is_active)
+
+
+
+
+
+
+# api section
+@app.route('/user_list')
 def users():
     output = os.popen('sudo occtl -j show users').read()
     
     output = json.loads(output)
     
     return output
-    
-    
-    
+
+
+
+
+
+# app runing
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
