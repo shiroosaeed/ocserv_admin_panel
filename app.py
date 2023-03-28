da2765ce9090c8b69d8a408348f1f51ec8e0440d
shiroosaeed <shiroosaeed@gmail.com>
2023-03-28 14:20:18 +0430

    v1.2.1

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template, jsonify
 import os
 import json
 
 from helpers import is_active
+
 app = Flask(__name__)
 
 app.config['template_engine'] = None
- 
+
 
 # load template section
 @app.route('/')
 def home():
     return render_template('index.html', is_active=is_active)
 
-@app.route('/users_list')  
+
+@app.route('/users_list')
 def users_list():
-     return render_template('users-list.html', is_active=is_active)
-
-
-
-
+    return render_template('users-list.html', is_active=is_active)
 
 
 # api section
 @app.route('/users')
 def users():
     output = os.popen('sudo occtl -j show users').read()
-    
+
     output = json.loads(output)
-    
+    users = []
+
+    for user in output:
+        user_new = []
+        for i in user:
+            user_new.push(user.replace(" ", '-'))
+        users.push(user_new)
+
     return output
-
-
-
 
 
 # app runing
